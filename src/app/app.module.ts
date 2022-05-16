4dacf918666f40b3ffb51ca10a44c07d08f648dd
hengtan <hengtan@live.com>
2022-05-16 00:37:14 -0300

    feat:refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { APP_BASE_HREF } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
+import { registerLocaleData } from "@angular/common";
+import localePt from "@angular/common/locales/pt";
+registerLocaleData(localePt);
 
 import { AppComponent } from "./app.component";
 import { MenuComponent } from "./navegacao/menu/menu.component";
